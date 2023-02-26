f2e72c554475fbc4ab6e92d4236fed8ecfe285fa
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2023-02-26 19:21:17 +0200

    added workshop project

---
+++ 04. Workshop Components/React-User-List-Workshop/client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <main className='main'>
+                <section className="card users-container">
+
+                </section>
+            </main>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
