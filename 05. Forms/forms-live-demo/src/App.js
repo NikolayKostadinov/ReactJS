a9068d4b8df9126fac6493b682f907735570407c
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2023-02-28 17:14:01 +0200

    added useRef

--- 05. Forms/forms-live-demo/src/App.js
+++ 05. Forms/forms-live-demo/src/App.js
@@ -1,6 +1,21 @@
-import { useState } from 'react';
+import { useRef } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 function App() {
+    const infoRef = useRef();
+
+    const dynamicFields = {
+        "individual": () =>
+            <div>
+                <label htmlFor="egn">EGN: </label>
+                <input type="text" name="egn" value={formState.egn} onChange={changeHandler} />
+            </div>,
+        "corporate": () =>
+            <div>
+                <label htmlFor="eik">EIK: </label>
+                <input type="text" name="eik" value={formState.eik} onChange={changeHandler} />
+            </div>
+    }
 
     const [formState, setFormState] = useState({
         username: '',
@@ -9,8 +24,17 @@ function App() {
         age: '',
         gender: 'f',
         userType: 'individual',
-        tac: false
+        tac: false,
+        egn: '',
+        eik: ''
     });
+
+    useEffect(() => {
+        if (formState.username && formState.age) {
+            infoRef.current.value = `${formState.username} - ${formState.age}`
+        }
+    }, [formState.username, formState.age]);
+
 
     function submitHandler(e) {
         e.preventDefault();
@@ -25,14 +49,6 @@ function App() {
                 : e.target.value
         });
     }
-
-    // function changeCheckHandler(e) {
-    //     setFormState({
-    //         ...formState,
-    //         [e.target.name]: e.target.checked
-    //     });
-    // }
-
 
     return (
         <div className="App">
@@ -119,6 +135,11 @@ function App() {
                     </div>
 
                     <div>
+                        {dynamicFields[formState.userType]()}
+                    </div>
+
+
+                    <div>
                         <label htmlFor="tac">Terms and Conditions:</label>
                         <input
                             type="checkbox"
@@ -128,7 +149,12 @@ function App() {
                             onChange={changeHandler} />
                     </div>
 
-                    <input type="submit" value="Login" />
+                    <input type="submit" value="Login" disabled={!formState.tac} />
+
+                    <div>
+                        <label htmlFor="unc-input">Uncontrolled Input: </label>
+                        <input type="text" id='unc-input' name="uncontrolled" ref={infoRef} />
+                    </div>
                 </form>
             </header>
         </div>
