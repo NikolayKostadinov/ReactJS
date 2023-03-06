a1b5eee74de6b9404221263375dbbcc98f1aafc0
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2023-03-06 19:26:47 +0200

    add routes and pages

--- 07.Workshop Routing/client/src/App.js
+++ 07.Workshop Routing/client/src/App.js
@@ -1,8 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Catalog from './components/catalog/Catalog';
+import CreateGame from './components/create-game/CreateGame';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
-
+import Login from './components/login/login';
+import Register from './components/register/Register';
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
         <main id="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/regirter" element={<Register />} />
+            <Route path="/create" element={<CreateGame />} />
+            <Route path="/catalog" element={<Catalog />} />
           </Routes>
         </main>
       </div>
