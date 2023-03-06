266095a2a75f7104e5b4ace837389e9640168a62
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2023-03-06 18:33:52 +0200

    created games

---
+++ 07.Workshop Routing/client/src/services/gameService.js
@@ -0,0 +1,7 @@
+const baseUrl = 'http://localhost:3030';
+
+export const  getAll = ()=>{
+    const url = `${baseUrl}/data/games?sortBy=_createdOn%20desc&distinct=category`;
+    return fetch(url)
+    .then(res => res.json());
+}
