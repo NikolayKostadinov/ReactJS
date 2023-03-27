9dd26843d1b71120b7ef606d02fa4971d51919f3
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2023-03-27 19:11:46 +0300

    Changes

--- 11. Workshop/jokes/src/components/joke-article/JokeArticle.js
+++ 11. Workshop/jokes/src/components/joke-article/JokeArticle.js
@@ -11,7 +11,7 @@ const JokeArticle = ({
                 <img src={iconUrl} alt="Chuck Norris Joke" />
             </header>
             <main>
-                <p className="joke-test">{text}</p>
+                <p className="joke">{text}</p>
             </main>
         </article>)
 }
